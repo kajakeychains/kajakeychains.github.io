11613b1ff68fd63fffa46cee270d90f3c66f8af9
MohammedAmalAlRaihan <orangtan3422@gmail.com>
2025-06-12 15:03:31 +1000

    added filter buttons

--- src/components/ProductBrowser/ProductBrowser.jsx
+++ src/components/ProductBrowser/ProductBrowser.jsx
@@ -1,37 +1,46 @@
 import { useEffect, useState } from "react";
 
 import ProductCard from "../ProductCard/ProductCard";
+import ProductFilters from "./ProductFilters";
 import './product-browser.css'
 
 function ProductBrowser() {
     const [keychains, setKeychains] = useState({});
-    let currentFilter = "minecraft"
+    const [currentFilter, setFilter] = useState("");
 
     useEffect(() => {
-        fetch('/product-information.json')
+        let jsonAddr = './product-information.json'
+        fetch(jsonAddr)
             .then((res) => res.json())
             .then((data) => setKeychains(data))
-            .catch((err) => console.error("Failed to load JSON:", err));
+            .catch((err) => console.error(`Failed to load ${jsonAddr}:`, err));
     }, []);
 
+    const handleFilterButtons = (value) => {
+        setFilter(value);
+    };
+
     return (
-        <div className="product-browser">
-            {Object.entries(keychains).map(([id, item]) => {
-                if (item.franchise === currentFilter) {
-                    return (
-                        <ProductCard
-                            key={id}
-                            name={item.name}
-                            price={item.price}
-                            franchise={item.franchise}
-                            stock={item.stock}
-                        />
-                    );
-                } else {
-                    return null;
-                }
-            })}
-        </div>
+        <>
+            <ProductFilters onSelect={handleFilterButtons} />
+            <div className="product-browser">
+                {Object.entries(keychains).map(([id, item]) => {
+                    if (item.franchise === currentFilter || currentFilter === "") {
+                        return (
+                            <ProductCard
+                                key={id}
+                                name={item.name}
+                                price={item.price}
+                                franchise={item.franchise}
+                                stock={item.stock}
+                            />
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+        </>
     );
 }
 
