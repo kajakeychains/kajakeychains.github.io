412bac6420d229f5e3fa9867d47b48d99415c99f
MohammedAmalAlRaihan <orangtan3422@gmail.com>
2025-06-15 16:41:28 +1000

    added mobile styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
                     <ul>
                         <li>Come to our store on <b>Business/Multicultural day!</b> (Thursday, 26th July)</li>
                         <li>We sell a <b>wide range of keychains</b> from popular media, such as <b>Minecraft</b> and <b>Jujutsu Kaisen</b></li>
-                        <li><b>Located on the (hopefully side) of the hall.</b></li>
+                        <li><b>Located in the hall.</b></li>
                     </ul>
                 </div>
             </div>
