f2ec6f9227ebe11e86feb3d95ac6fb893d530b2b
MohammedAmalAlRaihan <orangtan3422@gmail.com>
2025-06-16 12:59:36 +1000

    restructured json, updated prices and names

--- src/components/ProductBrowser/ProductBrowser.jsx
+++ src/components/ProductBrowser/ProductBrowser.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
-
 import ProductCard from "../ProductCard/ProductCard";
 import ProductFilters from "./ProductFilters";
-import './product-browser.scss'
+import './product-browser.scss';
 
 function ProductBrowser() {
-    const [keychains, setKeychains] = useState({});
+    const [keychains, setKeychains] = useState(null);
     const [currentFilter, setFilter] = useState("all");
 
     useEffect(() => {
-        let jsonAddr = './product-information.json'
+        let jsonAddr = '/product-information.json';
         fetch(jsonAddr)
             .then((res) => res.json())
             .then((data) => setKeychains(data))
@@ -20,25 +19,28 @@ function ProductBrowser() {
         setFilter(value);
     };
 
+    if (!keychains || !keychains.data || !keychains.prices) {
+        return <div>Loading products...</div>;
+    }
+
     return (
         <>
             <ProductFilters onSelect={handleFilterButtons} />
             <div className="product-browser">
-                {Object.entries(keychains).map(([id, item]) => {
+                {Object.entries(keychains.data).map(([id, item]) => {
                     if (item.franchise === currentFilter || currentFilter === "all") {
                         return (
                             <ProductCard
                                 key={id}
                                 id={id}
                                 name={item.name}
-                                price={item.price}
+                                price={keychains.prices[item.franchise] || 0}
                                 franchise={item.franchise}
                                 stock={item.stock}
                             />
                         );
-                    } else {
-                        return null;
                     }
+                    return null;
                 })}
             </div>
         </>
